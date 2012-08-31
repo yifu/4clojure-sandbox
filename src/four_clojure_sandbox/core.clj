2f287751a4ebb196bae874d4d38113578f4554de
Yves Baumes <ybaumes@gmail.com>
2012-08-31 22:05:22 +0200

    Lein init

---
+++ src/four_clojure_sandbox/core.clj
@@ -0,0 +1,6 @@
+(ns four-clojure-sandbox.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/four_clojure_sandbox/core_test.clj
@@ -0,0 +1,7 @@
+(ns four-clojure-sandbox.core-test
+  (:use clojure.test
+        four-clojure-sandbox.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
