ab37a70f5f6be3ef3e36ca3530bfe0f15bb43695
Yves Baumes <ybaumes@gmail.com>
2012-09-02 01:09:25 +0200

    Refine solution to problem 44.

--- src/four_clojure_sandbox/problem-44.clj
+++ src/four_clojure_sandbox/problem-44.clj
@@ -5,7 +5,40 @@
 (defn rot-x [x l]
   (last (take (inc (mod x (count l))) (iterate l-shift l))))
 
+(defn rot-n [x l]
+  (let [n (mod x (count l))]
+        (reee (nthrest l n) (take n l))))
+
+(defn rot-n [x l]
+  (let [n (mod x (count l))]
+       `( ~@(nthrest l n) ~@(take n l))))
+
+(defn rot-n [x l]
+  ( (fn [n] `( ~@(nthrest l n) ~@(take n l)) ) (mod x (count l))))
+
+(defn rot-n [x l]
+  #( `( (list ~@(nthrest l %) ~@(take % l))) ) (mod x (count l)))
+
+
+(rot-n  2 '(:a :b :c :d :e :f :g :h))
+
+(def seq-appends
+  #(`(~@ % ~@ %1)))
+
+(defn reee [l r]
+  #(`(~@l ~@r)))
+
+(seq-appends '(1 2) '(3 4 5 6))
+
+(take 2 '(1 2 3 4 5 6))
+(nthrest '(1 2 3 4 5 6) 2)
+
 (def __ rot-x)
+
+;; Solution
+(def __ 
+  #( (fn [n] `( ~@(nthrest %2 n) ~@(take n %2)) ) (mod % (count %2))))
+
 
 (and (= (__ 2 [1 2 3 4 5]) '(3 4 5 1 2))
      (= (__ -2 [1 2 3 4 5]) '(4 5 1 2 3))
