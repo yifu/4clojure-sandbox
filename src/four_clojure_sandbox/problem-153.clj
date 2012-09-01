c9e09f80942fec0a544ac341206be3301a4267d9
Yves Baumes <ybaumes@gmail.com>
2012-09-01 23:07:18 +0200

    Problem 153.

---
+++ src/four_clojure_sandbox/problem-153.clj
@@ -0,0 +1,56 @@
+(ns four-clojure-sandbox.core)
+
+(def __ (fn [ss]
+          (empty?
+           (apply clojure.set/union
+                  (for [s ss
+                        o (clojure.set/difference ss #{ s })]
+                    (clojure.set/intersection s o) ) ))))
+
+(def __ (fn [s]
+          (apply clojure.set/intersection s)))
+
+(and
+ (= (__ #{#{\U} #{\s} #{\e \R \E} #{\P \L} #{\.}})
+    true)
+
+ (= (__ #{#{:a :b :c :d :e}
+         #{:a :b :c :d}
+         #{:a :b :c}
+         #{:a :b}
+         #{:a}})
+    false)
+ 
+ (= (__ #{#{[1 2 3] [4 5]}
+         #{[1 2] [3 4 5]}
+         #{[1] [2] 3 4 5}
+         #{1 2 [3 4] [5]}})
+    true)
+ 
+ (= (__ #{#{'a 'b}
+         #{'c 'd 'e}
+         #{'f 'g 'h 'i}
+         #{''a ''c ''f}})
+    true)
+ (= (__ #{#{'(:x :y :z) '(:x :y) '(:z) '()}
+         #{#{:x :y :z} #{:x :y} #{:z} #{}}
+         #{'[:x :y :z] [:x :y] [:z] [] {}}})
+    false)
+ (= (__ #{#{(= "true") false}
+         #{:yes :no}
+         #{(class 1) 0}
+         #{(symbol "true") 'false}
+         #{(keyword "yes") ::no}
+         #{(class '1) (int \0)}})
+    false)
+ (= (__ #{#{distinct?}
+         #{#(-> %) #(-> %)}
+         #{#(-> %) #(-> %) #(-> %)}
+         #{#(-> %) #(-> %) #(-> %)}})
+    true)
+ (= (__ #{#{(#(-> *)) + (quote mapcat) #_ nil}
+         #{'+ '* mapcat (comment mapcat)}
+         #{(do) set contains? nil?}
+         #{, , , #_, , empty?}})
+    false) )
+ 
