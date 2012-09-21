65bd84255cdabc02c52c679380f8f4500ab33f72
Yves Baumes <ybaumes@gmail.com>
2012-09-21 20:01:40 +0200

    First solution to problem 112.

--- src/four_clojure_sandbox/problem-112.clj
+++ src/four_clojure_sandbox/problem-112.clj
@@ -24,6 +24,7 @@
         (zip/replace loc '/)
         loc)))))
 
+
 (def  __
   (fn [a s]
     (loop [n a z (zip/vector-zip s)]
@@ -41,6 +42,51 @@
               (zip/remove z)
               z))))))))
 
+(def __
+  (fn [n s]
+    (loop [n n z (zip/vector-zip s)]
+      (if (or (zip/end? z) (neg? n))
+        (zip/path z)
+        (recur (if (number? (zip/node z)) (- n (zip/node z)) n ) (zip/next z))))))
+
+(defn get-n [s]
+  (if (empty? s)
+    0
+    (if (number? (first s))
+      (+ (first s) (get-n (rest s)))
+      (+ (get-n (first s)) (get-n (rest s))))))
+
+(get-n [1 2 3])
+
+(def __
+  (fn f [n s]
+    (if (empty? s)
+      '()
+      (if (number? (first s))
+        (if (<= 0 (- n (first s)))
+          (cons (first s) (f (- n (first s)) (rest s)))
+          '())
+        (cons (f n (first s)) (f (- n (get-n (first s))) (rest s)))))))
+
+(defn get-n [s]
+  (if (number? s)
+    s
+    (if-let [[a & r] s]
+      (+ (get-n a) (get-n r))
+      0)))
+
+(get-n 3)
+(get-n [1 2 3])
+(get-n [[1 [ 2 ]] 3])
+
+(def __
+  (fn f [n [a & r]]
+    (let [g (fn g [s] (if (number? s) s (if-let [[a & r] s] (+ (g a) (g r)) 0)))]
+     (if a
+       (if (number? a)
+         (if (neg? (- n a)) '() (cons a (f (- n a) r)))
+         (cons (f n a) (f (- n (g a)) r)))
+       '()))))
 
 (and (=  (__ 10 [1 2 [3 [4 5] 6] 7])
          '(1 2 (3 (4))))
