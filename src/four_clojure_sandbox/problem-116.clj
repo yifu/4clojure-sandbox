b691f419124f04bf4b6cbf4fd8c10b6e4d793114
Yves Baumes <ybaumes@gmail.com>
2012-09-22 23:01:17 +0200

    Final solution to problem 116.

--- src/four_clojure_sandbox/problem-116.clj
+++ src/four_clojure_sandbox/problem-116.clj
@@ -45,6 +45,16 @@
                                      (or (and c d) (and (not c) (not d) (recur (inc x))))))
                                  false))))
 
+(def ^{:private true} __ (fn [n]
+                           (let [p (fn [n] (if (> n 1) (not-any? zero? (for [x (range 2 n)] (mod n x)))))]
+                               (if (p n)
+                                 ((fn [x]
+                                    (let [c (p (- n x))
+                                          d (p (+ n x))]
+                                      (or (and c d) (and (not c) (not d) (recur (inc x)))))) 1)
+                                 false))))
+
+
 (= nil false)
 (prime-p 1)
 (prime-p 3)
@@ -59,72 +69,60 @@
      (= true (__ 563))
      (= 1103 (nth (filter __ (range)) 15)))
 
-(def myvar balanced-prime? )
 
-(load-string "(defn prime-p [n]
-  (if (> n 1)  
-    (not-any? zero? (for [x (range 2 n)] (mod n x)))))
-")
+;; Second solution from scratch to avoid the timeout issue.
 
-(for   [ x  (read-string "(defn prime-p [n]
-  (if (> n 1)  
-    (not-any? zero? (for [x (range 2 n)] (mod n x)))))
-")] (prn x))
+(defn- prime? [n]
+  (and
+   (> n 1)  
+   (not-any? zero? (for [x (range 2 n)] (mod n x)))))
 
-(read-string "#()")
+(defn is-a-prime [p]
+  (and (> p 1)
+       (every? true?
+               (map #(not= 0 (mod p %)) (range 2 p)))))
 
+(for [i (range 0 15)] [ i (prime? i)])
+(is-a-prime 0)
 
-(prn (read-string "(defn balanced-prime? [n]
-  (if (prime-p n)
-    (loop [x 1]
-      (let [a (- n x)
-            b (+ n x)
-            p-a (prime-p a)
-            p-b (prime-p b)]
-        (comment (if (and p-a p-b)
-                   true
-                   (if (not (or p-a p-b))
-                     (recur (inc x))
-                     false)))
-        (or (and p-a p-b) (and (not p-a) (not p-b) (recur (inc x))))))
-    false))
-"))
+(defn- a-prime [n o]
+  (if (pos? n)
+    (if (prime? (o n)) (o n) (recur (o n) o))
+    n))
+
+(defn- n-prime [n] (a-prime n inc))
+(defn- p-prime [n] (a-prime n dec))
+
+(n-prime 13)
+(p-prime 13)
+(p-prime 1)
 
 
-(defn shorten-list [l]
-  (apply str  (for [x l] (shorten-it x))))
+(defn- balanced-prime? [n]
+  (and
+   (prime? n)
+   (= n (/ (+ (n-prime n) (p-prime n)) 2))))
+
+(def __ balanced-prime?)
+
+(balanced-prime? 564)
+(for [i (range 0 15)] [i (balanced-prime? i)])
+
+(defn __ [i]
+  (let [p (fn [p] (and (> p 1)
+                   (every? true?
+                           (map #(not= 0 (mod p %)) (range 2 p)))))
+        a (fn [n o]
+            (if (pos? n)
+              (if (p (o n)) (o n) (recur (o n) o))
+              n))
+        n (fn [n] (a n inc))
+        r (fn [n] (a n dec))]
+    (and
+     (p i)
+     (= i (/ (+ (n i) (r i)) 2)))))
 
 
-(defn shorten-it [elt]
-  (let [core? #{'defn 'if 'for 'let 'loop 'and 'or 'recur}]
-   (cond
-    (list? elt) (str "(" (shorten-list elt) ")")
-    (vector? elt) (str "[" (shorten-list elt) "]")
-    (core? elt) (str elt " ")
-    (clojure.test/function? elt) (str elt " ")
-    (keyword? elt) (str "kwd " elt " ")
-    (symbol? elt) (str (first (name elt)) " ")
-    :else (str elt " "))))
-
-(load-string (shorten-it '
-   (defn prime-p [nbr]
-     (if (> nbr 1)  
-       (not-any? zero? (for [elt (range 2 nbr)] (mod nbr elt)))))
-   ))
-
-(shorten-it (read-string "(defn balanced-prime? [n]
-  (if (prime-p n)
-    (loop [x 1]
-      (let [a (- n x)
-            b (+ n x)
-            p-a (prime-p a)
-            p-b (prime-p b)]
-        (comment (if (and p-a p-b)
-                   true
-                   (if (not (or p-a p-b))
-                     (recur (inc x))
-                     false)))
-        (or (and p-a p-b) (and (not p-a) (not p-b) (recur (inc x))))))
-    false))
-"))
-
+(and (= false (__ 4))
+     (= true (__ 563))
+     (= 1103 (nth (take 20 (filter __ (range))) 15)))
